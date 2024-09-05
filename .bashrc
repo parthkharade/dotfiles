843f0eb80ead4a7b3131de56b68b2b222f90ebb4
Parth Kharade <parthkharade99@gmail.com>
2024-09-05 14:20:56 -0600

    Cleaned up tmux conf and bashrc

--- .bashrc
+++ .bashrc
@@ -155,7 +155,6 @@ eval "$(zoxide init bash)"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-. "$HOME/.cargo/env"
 
 
 if [ hostname == "c3d" ]; then
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/parth_kharade/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?.lua;/home/parth_kharade/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?/init.lua;/home/parth_kharade/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?.lua;/home/parth_kharade/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/parth_kharade/.cache/nvim/packer_hererocks/2.1.1713484068/lib/lua/5.1/?.so"
+local package_path_str = "/home/parth/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?.lua;/home/parth/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?/init.lua;/home/parth/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?.lua;/home/parth/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/parth/.cache/nvim/packer_hererocks/2.1.1713484068/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,93 +76,107 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["coc.nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/coc.nvim",
     url = "https://github.com/neoclide/coc.nvim"
   },
   ["darkearth-nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/darkearth-nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/darkearth-nvim",
     url = "https://github.com/ptdewey/darkearth-nvim"
   },
   ["gruvbox.nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
     url = "https://github.com/ellisonleao/gruvbox.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["lush.nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/lush.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/lush.nvim",
     url = "https://github.com/rktjmp/lush.nvim"
   },
   ["miasma.nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/miasma.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/miasma.nvim",
     url = "https://github.com/xero/miasma.nvim"
+  },
+  ["mini.nvim"] = {
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/parth/.local/share/nvim/site/pack/packer/opt/mini.nvim",
+    url = "https://github.com/echasnovski/mini.nvim"
   },
   ["minintro.nvim"] = {
     config = { "\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rminintro\frequire\0" },
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/minintro.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/minintro.nvim",
     url = "https://github.com/eoh-bse/minintro.nvim"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/nvim-tree/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   okai = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/okai",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/okai",
     url = "https://github.com/okaihe/okai"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["render-markdown.nvim"] = {
+    config = { "\27LJ\2\nA\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\20render-markdown\frequire\0" },
+    load_after = {},
+    loaded = true,
+    needs_bufread = false,
+    path = "/home/parth/.local/share/nvim/site/pack/packer/opt/render-markdown.nvim",
+    url = "https://github.com/MeanderingProgrammer/render-markdown.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-commentary"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/vim-commentary",
     url = "https://github.com/tpope/vim-commentary"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-python-pep8-indent"] = {
     loaded = true,
-    path = "/home/parth_kharade/.local/share/nvim/site/pack/packer/start/vim-python-pep8-indent",
+    path = "/home/parth/.local/share/nvim/site/pack/packer/start/vim-python-pep8-indent",
     url = "https://github.com/Vimjas/vim-python-pep8-indent"
   }
 }
@@ -172,6 +186,15 @@ time([[Defining packer_plugins]], false)
 time([[Config for minintro.nvim]], true)
 try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rminintro\frequire\0", "config", "minintro.nvim")
 time([[Config for minintro.nvim]], false)
+-- Load plugins in order defined by `after`
+time([[Sequenced loading]], true)
+vim.cmd [[ packadd nvim-treesitter ]]
+vim.cmd [[ packadd render-markdown.nvim ]]
+
+-- Config for: render-markdown.nvim
+try_loadstring("\27LJ\2\nA\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\20render-markdown\frequire\0", "config", "render-markdown.nvim")
+
+time([[Sequenced loading]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
